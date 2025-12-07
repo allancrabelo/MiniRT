62ea7206a730ef136813146ea1c9e978e679e6dd
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-12-07 16:56:23 +0000

    Cylinders interssect fixed

--- src/rendering/intersect_cylinder.c
+++ src/rendering/intersect_cylinder.c
@@ -15,6 +15,47 @@ static bool	check_cylinder_caps(t_ray ray, t_obj *obj, float t, t_vector normal)
 	return (true);
 }
 
+static t_hit	intersect_cap(t_ray ray, t_obj *obj, float cap_height, t_vector axis)
+{
+	t_vector	cap_center;
+	float		denom;
+	float		t;
+	t_vector	p;
+	t_vector	v;
+	float		dist_sq;
+	float		radius;
+	t_hit		hit;
+
+	cap_center = vector_add(obj->objects.cylinder.coordinates, 
+		vector_mult(axis, cap_height));
+	denom = vector_dot(axis, ray.direction);
+	
+	if (fabs(denom) < 0.0001)
+		return ((t_hit){.hit = false, .t = INFINITY});
+	
+	t = vector_dot(vector_sub(cap_center, ray.origin), axis) / denom;
+	
+	if (t < 0.001)
+		return ((t_hit){.hit = false, .t = INFINITY});
+	
+	p = vector_add(ray.origin, vector_mult(ray.direction, t));
+	v = vector_sub(p, cap_center);
+	radius = obj->objects.cylinder.diameter / 2.0;
+	dist_sq = vector_dot(v, v);
+	
+	if (dist_sq > radius * radius)
+		return ((t_hit){.hit = false, .t = INFINITY});
+	
+	hit.hit = true;
+	hit.t = t;
+	hit.point = p;
+	hit.object = obj;
+	hit.normal = axis;
+	if (denom > 0)
+		hit.normal = vector_mult(axis, -1);
+	return (hit);
+}
+
 static t_hit	cylinder_body_intersect(t_ray ray, t_obj *obj)
 {
 	t_vector	oc;
@@ -67,12 +108,30 @@ static t_hit	cylinder_body_intersect(t_ray ray, t_obj *obj)
 t_hit	intersect_cylinder(t_ray ray, t_obj *obj)
 {
 	t_hit		hit;
+	t_hit		cap_hit;
 	t_vector	d;
 	t_vector	p;
+	t_vector	axis;
 
+	axis = obj->objects.cylinder.orientation;
 	hit = cylinder_body_intersect(ray, obj);
+	
+	cap_hit = intersect_cap(ray, obj, 0, axis);
+	if (cap_hit.hit && cap_hit.t < hit.t)
+		hit = cap_hit;
+	
+	cap_hit = intersect_cap(ray, obj, obj->objects.cylinder.height, axis);
+	if (cap_hit.hit && cap_hit.t < hit.t)
+		hit = cap_hit;
+	
 	if (!hit.hit)
 		return (hit);
+	
+	if (hit.normal.x != 0 || hit.normal.y != 0 || hit.normal.z != 0)
+	{
+		if (fabs(vector_dot(hit.normal, axis)) > 0.99)
+			return (hit);
+	}
 	
 	p = hit.point;
 	d = vector_sub(p, obj->objects.cylinder.coordinates);
